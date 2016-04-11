c80467d4789216a86c65b576fde4ab972c8b2070
BuildTools <mreilaender@student.tgm.ac.at>
2016-04-11 20:03:44 +0200

    add find indeces for given header

--- src/model.py
+++ src/model.py
@@ -32,3 +32,19 @@ class Model(object):
     def load_config_from_json(self, filename):
         with open(filename, 'r') as file:
             return json.load(file)
+
+    def find_indeces(self, header):
+        """
+        find the indeces for some items in the given header and returns them as a dictionary
+
+        :param header: Array Header of the table model
+        :return:
+        """
+        indeces = {'T': None, 'WV': None, 'WK': None, 'BZ': None, 'SPR': None,
+                        'WBER': None, ' ABG.': None, 'UNG.': None, 'SPOE': None,
+                        'FPOE': None, 'OEVP': None, 'GRUE': None, 'NEOS': None,
+                        'WWW': None, 'ANDAS': None, 'GFW': None, 'SLP': None,
+                        'WIFF': None, 'M': None, 'FREIE': None}
+        for index, item in enumerate(header):
+            indeces[item.replace(" ", "")] = index
+        return indeces

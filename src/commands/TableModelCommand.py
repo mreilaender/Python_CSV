b6f5c4206ebf00140779b9c0b60f626e9f0773bd
BuildTools <mreilaender@student.tgm.ac.at>
2016-04-15 09:00:17 +0200

    implement QUndoCommand for table model

---
+++ src/commands/TableModelCommand.py
@@ -0,0 +1,28 @@
+from PySide.QtGui import QUndoCommand
+
+
+class TableModelCommand(QUndoCommand):
+    """
+    Handles undo/redo functionality of a QAbstractTableModel
+
+    """
+    def __init__(self, table_model, index, old, new):
+        """
+        Constructor
+
+        :param table_model: QAbstractTableModel TableModel where data bas been changed
+        :param index: QModelIndex
+        :param old: QVariant Old TableModel data
+        :param new: QVariant New TableModel data
+        """
+        super().__init__()
+        self.table_model = table_model
+        self.old = old
+        self.new = new
+        self.index = index
+
+    def undo(self):
+        self.table_model.setData(self.index, self.old, None)
+
+    def redo(self):
+        self.table_model.setData(self.index, self.new, None)
